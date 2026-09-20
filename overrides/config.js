10b96e2540f8e9380f93978139842398d1794424
wlsdn8842-cyber <wlsdn8842@gmail.com>
2026-09-20 23:10:34 +0900

    Add Supabase feedback configuration

---
+++ overrides/config.js
@@ -0,0 +1,28 @@
+window.CMH_CONFIG = {
+  GAME_TITLE: 'Capture My Heart! Territory Panic',
+  VERSION: '0.5.2',
+  GAMEPLAY: {
+    DANGER_BGM_ENABLED: false,
+    DANGER_VISUAL_ENABLED: true,
+    CAMERA_ZOOM: 1.50,
+    MINIMAP_ENABLED: true
+  },
+  ADS: {
+    ENABLED: false,
+    PUBLISHER_ID: '',
+    TEST_MODE: false,
+    FREQUENCY_HINT: '120s',
+    INTERSTITIAL_AFTER_STAGES: [2, 4, 6, 8],
+    MIN_SECONDS_BETWEEN_INTERSTITIALS: 120
+  },
+  FEEDBACK: {
+    ENABLED: true,
+    ENDPOINT: 'https://cvfmikycscmfjmooxhni.supabase.co/rest/v1/cmh_feedback',
+    API_KEY: 'sb_publishable_AlQwZgMrOCznUo4LQePZiw_maIjWv6w',
+    REQUEST_TIMEOUT_MS: 6500,
+    MAX_LOCAL_QUEUE: 50
+  },
+  ANALYTICS: {
+    GA_MEASUREMENT_ID: ''
+  }
+};

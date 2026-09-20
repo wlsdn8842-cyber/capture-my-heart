88bf34a1a4b61612ba9e5d5c34aa68aae274c3f0
wlsdn8842-cyber <wlsdn8842@gmail.com>
2026-09-20 23:35:57 +0900

    Refresh v0.5.3 service worker cache

--- overrides/sw.js
+++ overrides/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cmh-core-v0.5.2';
+const CACHE='cmh-core-v0.5.3';
 const CORE=['./','index.html','styles.css','config.js','ads.js','feedback.js','game.js','manifest.webmanifest','assets/images/title.webp','assets/audio/main.mp3'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

6fe6f1deb43706f88b99f841aa4e4224802c1cc7
wlsdn8842-cyber <wlsdn8842@gmail.com>
2026-09-22 21:47:15 +0900

    feat: add Stage 4 Miko bonus scene

--- overrides/config.js
+++ overrides/config.js
@@ -1,6 +1,6 @@
 window.CMH_CONFIG = {
   GAME_TITLE: 'Capture My Heart! Territory Panic',
-  VERSION: '0.6.8',
+  VERSION: '0.6.9',
   GAMEPLAY: {
     DANGER_BGM_ENABLED: false,
     DANGER_VISUAL_ENABLED: true,
